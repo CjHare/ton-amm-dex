34b7ea864f24af936660da8718d85509c455cde8
cjhare <CjHare@users.noreply.github.com>
2024-08-22 11:06:44 +1000

    feat: Pool test setup

--- tests/Pool.spec.ts
+++ tests/Pool.spec.ts
@@ -1,14 +1,22 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { Cell, toNano } from '@ton/core';
+import { Address, Cell, toNano } from '@ton/core';
 import { Pool } from '../wrappers/Pool';
-import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
+import { randomAddress } from './lib/helpers';
+
+/** Import the TON matchers */
+import "@ton/test-utils";
 
 describe('Pool', () => {
-    let code: Cell;
+    let poolCode: Cell;
+    let walletCode: Cell;
+    let accountCode: Cell;
+    let routerAddress: Address;
 
     beforeAll(async () => {
-        code = await compile('Pool');
+        poolCode = await compile('Pool');
+        walletCode = await compile('Wallet');
+        accountCode = await compile('Account');
     });
 
     let blockchain: Blockchain;
@@ -18,7 +26,24 @@ describe('Pool', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        pool = blockchain.openContract(Pool.createFromConfig({}, code));
+        routerAddress = randomAddress("a valid pool");
+
+        pool = blockchain.openContract(Pool.createFromConfig({        
+            routerAddress: routerAddress,
+            lpFee: BigInt(20),
+            protocolFee: BigInt(0),
+            refFee: BigInt(10),
+            protocolFeesAddress: randomAddress("a valid protocol fee address"),
+            collectedTokenAProtocolFees: BigInt(0),
+            collectedTokenBProtocolFees: BigInt(0),
+            wallet0: randomAddress("wallet0"),
+            wallet1: randomAddress("wallet1"),
+            reserve0: BigInt(0),
+            reserve1: BigInt(0),
+            supplyLP: BigInt(0),
+            LPWalletCode: walletCode,
+            LPAccountCode: accountCode
+        }, poolCode));
 
         deployer = await blockchain.treasury('deployer');
 
